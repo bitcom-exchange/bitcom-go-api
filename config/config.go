c91eb720f408e9f221fe53a61a29e144183997fb
huahui.gao <huahui.gao@matrixport.com>
2021-02-01 19:58:07 +0800

    remove host

--- config/config.go
+++ config/config.go
@@ -1,24 +1,8 @@
 package config
 
-// Wafei Alpha
-var User1Host = "https://alphaapi.bitexch.dev"
-var User1Uid = "475183"
-var User1AccessKey = "ak-3d4dc159-8313-4726-860e-60355ccf1d24"
-var User1SecretKey = "A2I2HuUKqx7VnxQVvJhvowvmgQ3GgunDtdsHHyEjZoQXn14zmf8hB8MZzjlgGOFg"
+var User1Host = "User1Host"
+var User1Uid = "User1Uid"
+var User1AccessKey = "User1AccessKey"
+var User1SecretKey = "User1SecretKey"
 
-// Eric Alpha
-var User2Host = "https://alphaapi.bitexch.dev"
-var User2Uid = "51140"
-var User2AccessKey = "ak-96cc0cbd-c501-448f-a32d-21228bc9648f"
-var User2SecretKey = "IM2DHuw04GQSaS4SLnXlCf3zegw0Lgli0YwGXAcd3c1vEXYjFVBHz8aViUehRNji"
-
-// Wafei Beta
-var User3Host = "https://betaapi.bitexch.dev"
-var User3Uid = "441730"
-var User3AccessKey = "ak-78ec99c9-8668-4d1d-a0fc-95540408264c"
-var User3SecretKey = "mzYOAoqcLelMFq4O6cGtX2U20tq8Iw09giSPth5wXqbzzOnhcBpbPAhzZlaHZQod"
-
-// ws
-var WsHost = "wss://alphaws.bitexch.dev"
-
-//var WsHost = "wss://betaws.bitexch.dev"
+var WsHost = "WebsocketHost"
